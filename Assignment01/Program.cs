42a5bccdd56d8594b456ae801472a7518ee40241
Benjamin Smith <Smith-B9@ulster.ac.uk>
2019-10-29 10:59:44 +0000

    Implimented basic menu system and groundwork of switch block to handle function calling

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -11,27 +11,45 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
+            AttendanceModel attendanceModel = new AttendanceModel();
             using (var db = new AttendanceModel())
             {
-                //Console.Write("Enter a new module name: ");
-                //var moduleName = Console.ReadLine();
-
-                //var module = new Module { ModuleName = moduleName };
-                //db.Modules.Add(module);
-                //db.SaveChanges();
-
-                var query = from b in db.Modules
-                            orderby b.ModuleName
-                            select b;
-
-                Console.WriteLine("All modules in db:");
-                foreach (var item in query)
+                Console.WriteLine("Attendance Monitoring System " +
+                                "\n=============================");
+                while (true)
                 {
-                    Console.WriteLine(item.ModuleName);
-                }
+                    Console.WriteLine("Please select an option to continue");
+                    Console.WriteLine("(1) List all module codes & names");
+                    Console.WriteLine("(2) Enter a module code to list the staff name and and number of" +
+                        "\nthe instructor taking the module");
+                    Console.WriteLine("(3) Enter a module code to list the date and time of all learning" +
+                        "\nevents associated with that module");
+                    Console.WriteLine("(4) Enter a module code to list the student number and attendance" +
+                        "\nrecord for said student");
+                    Console.WriteLine("(5) Enter a module code and list the names of any students who have" +
+                        "\nmissed 2 or more classes for said module");
+                    Console.WriteLine("(6) Enter a module code to generate an attendance report");
+                    Console.WriteLine("(7) Exit the system");
 
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
+                    //Handle options selection
+                    int option = 0;
+                    Console.WriteLine("Enter Option Number: ");
+                    while (!(int.TryParse(Console.ReadLine(), out option) && (option >= 1 && option <= 7)))
+                    {
+                        Console.WriteLine("Option must be between 1 and 7");
+                        Console.WriteLine("Enter Option Number: ");
+                    }
+                    //Switch statement to call function based on which option was entered
+                    switch (option)
+                    {
+                        case 7:
+                            Console.WriteLine("Goodbye");
+                            return;
+                        default:
+                            Console.WriteLine("Invalid Option!");
+                            break;
+                    }
+                }//end while loop
             }
         }
     }
