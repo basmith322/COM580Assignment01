55a2d29d871ed1cc996e2615013086aec5ff946a
Benjamin Smith <Smith-B9@ulster.ac.uk>
2019-10-31 14:26:37 +0000

    Updated all outputs to be cleaner, fixed all validation including nulls.

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -18,6 +18,9 @@ namespace Assignment01
             {
                 while (true)
                 {
+                    //Clear previous results from console
+                    Console.Clear();
+                    // Create a table to hold a list of menu options
                     var table = new ConsoleTable("Attendance Monitoring System");
                     table.AddRow("Please Select an option to continue");
                     table.AddRow("(1) List all module codes & names");
@@ -33,61 +36,75 @@ namespace Assignment01
                     table.AddRow("(7) Exit the system");
                     table.Write();
 
-                    //Handle options selection
+                    // Handle options selection
                     int option = 0;
-                    Console.WriteLine("Enter Option Number: ");
+                    Console.Write("Enter Option Number: ");
                     while (!(int.TryParse(Console.ReadLine(), out option) && (option >= 1 && option <= 7)))
                     {
                         Console.WriteLine("Option must be between 1 and 7");
                         Console.WriteLine("Enter Option Number: ");
                     }
-                    //Switch statement to call function based on which option was entered
+
+                    // Switch statement to call function based on which option was entered
                     switch (option)
                     {
                         case 1:
+                            Console.Clear();
                             moduleAttendanceMenu.ListModules(db);
                             break;
                         case 2:
+                            Console.Clear();
                             moduleAttendanceMenu.InstructorTakingModule(db);
                             break;
                         case 3:
+                            Console.Clear();
                             moduleAttendanceMenu.ModuleLearningEvents(db);
                             break;
                         case 4:
+                            Console.Clear();
                             moduleAttendanceMenu.AttendanceRecord(db);
                             break;
                         case 5:
+                            Console.Clear();
                             moduleAttendanceMenu.MissedClasses(db);
                             break;
                         case 6:
+                            Console.Clear();
                             moduleAttendanceMenu.GenerateReport(db);
                             break;
                         case 7:
+                            Console.Clear();
                             Console.WriteLine("Goodbye");
                             return;
                         default:
+                            Console.Clear();
                             Console.WriteLine("Invalid Option!");
                             break;
                     }// end switch
-                }//end while loop
-            }//end using
-        }//end main
+                }// end while loop
+            }// end using
+        }// end main
 
         //(1)   List the module code and name of all Modules.
         public void ListModules(AttendanceModel dbC)
         {
-            var query = from b in dbC.Modules
-                        orderby b.ModuleName
-                        select b;
-            //Create a new table
+            Console.WriteLine("List of all modules \n");
+            // Initiate query to obtain all modules from the table
+            var query = from m in dbC.Modules
+                        orderby m.ModuleName
+                        select m;
+
+            // Create a new table to house the results
             var table = new ConsoleTable("Module Code", "Module Title");
 
+            // Ensure that the query doesn't return a blank result
             if (query.FirstOrDefault() == null)
             {
                 Console.WriteLine("There are no modules in the system");
                 ReturnToMenu();
             }
 
+            //Iterate through query results to add the moduleCode and moduleName to the table
             foreach (var item in query)
             {
                 table.AddRow(item.ModuleCode, item.ModuleName);
@@ -101,13 +118,20 @@ namespace Assignment01
         //(2)   Given a module code, list the “Staff Number” and “Name” of the instructor taking the module.
         public void InstructorTakingModule(AttendanceModel dbC)
         {
+            Console.WriteLine("List of Staff taking module \n");
+
+            /* Obtain the module code from the user, query it against the 
+            database and store the value in the moduleCode variable */
             string moduleCode = getModuleCode();
             var module = dbC.Modules.Where(m => m.ModuleCode == moduleCode).FirstOrDefault();
-            var instructors = module.Instructor;
+            
+
+            // Check that the module variable isn't empty and print the required information
             if (module != null)
             {
-                Console.WriteLine(String.Format("Module Code {0} | Instructor Name {1} | Instructor Number {2}",
-                    module.ModuleCode, instructors.StaffName, instructors.StaffNum));
+                var instructors = module.Instructor;
+                Console.WriteLine(String.Format("Module Code: {0} | Instructor Name: {1} | Instructor Number: {2}",
+                    module.ModuleCode, instructors.StaffName, instructors.StaffNum) + "\n");
             }
             else
             {
@@ -117,18 +141,26 @@ namespace Assignment01
         }
         //end InstructorTakingModule function
 
-        //(3)   Given a module code, list the “Date”, “Time” and “Type” of all the learning events associated with that module.
+        /* (3)   Given a module code, list the “Date”, “Time” and “Type” of 
+        all the learning events associated with that module. */
         public void ModuleLearningEvents(AttendanceModel dbC)
         {
-            string moduleCode = getModuleCode();
+            Console.WriteLine("List of learning events for module \n");
 
+            /* Obtain the module code from the user, query it against the 
+            database and store the value in the moduleCode variable */
+            string moduleCode = getModuleCode();
             var module = dbC.Modules.Where(l => l.ModuleCode == moduleCode).FirstOrDefault();
 
+            // Check that the module variable isn't empty and print the required information
             if (module != null)
             {
-                Console.WriteLine("Module Code: " + module.ModuleCode);
                 var events = module.LearningEvents;
+
+                //Create table
                 var table = new ConsoleTable("Event Type", "Event Date/Time");
+
+                // Iterate through the results adding a row for each set of results. Print the table
                 foreach (var learningEvent in events)
                 {
                     table.AddRow(learningEvent.eventType, learningEvent.eventDateTime);
@@ -143,12 +175,17 @@ namespace Assignment01
         }
         //end ModuleLearningEvents function
 
-        //(4)   Given a module code and student number, display the attendance record (ordered by date) for that student.
+        /* (4)   Given a module code and student number, display the attendance record 
+        (ordered by date) for that student. */
         public void AttendanceRecord(AttendanceModel dbC)
         {
+            Console.WriteLine("Student attendance for a module \n");
+
+            // Obtain the module code & student number from the user
             string moduleCode = getModuleCode();
             string studentNum = getStudentNum();
 
+            // Joining tables together to ensure the relevant information can be queried
             var query =
             from attendanceA in dbC.Attendances
             join learningEventA in dbC.LearningEvents on attendanceA.eventID equals learningEventA.Id
@@ -166,27 +203,40 @@ namespace Assignment01
                 studentA.studentSurname
             };
 
+            var table = new ConsoleTable("Student Name", "Learning Event", "Attendance Status", "Event Date/Time");
+
+            // Check the query results are not empty
             if (query.FirstOrDefault() == null)
             {
                 Console.WriteLine("Student Number or Module code not found");
                 ReturnToMenu();
             }
-            string fullname = query.FirstOrDefault().studentForname + " " + query.FirstOrDefault().studentSurname;
-            Console.WriteLine("The student number is incorrect, please enter a valid student number");
-
-            foreach (var attendances in query)
+            else
             {
-                Console.WriteLine(String.Format("{0} {1} Attendance: {2} on {3}", fullname, attendances.eventType, attendances.attendanceStatus, attendances.eventDateTime));
+                // String to concatenate student forename and surname
+                string fullname = query.FirstOrDefault().studentForname + " " + query.FirstOrDefault().studentSurname;
+
+                // Iterate through the results and write a line for each result
+                foreach (var attendances in query)
+                {
+                    table.AddRow(fullname, attendances.eventType, attendances.attendanceStatus, attendances.eventDateTime);
+                }
+                table.Write();
+                Console.WriteLine("");
+                ReturnToMenu();
             }
-            ReturnToMenu();
         }
         //end AttendanceRecord function
 
         //(5)   List the names of all students who have missed two or more learning experiences given the module code.
         public void MissedClasses(AttendanceModel dbC)
         {
+            Console.WriteLine("List of students who have missed more than 2 learning experiences \n");
+
+            // Obtain the module code from the user
             string moduleCode = getModuleCode();
 
+            // Joining tables together to ensure the relevant information can be queried
             var studentIds =
                 from attendanceA in dbC.Attendances
                 join learningEventA in dbC.LearningEvents on attendanceA.eventID equals learningEventA.Id
@@ -222,11 +272,14 @@ namespace Assignment01
                     studentA.studentNumber
                 };
 
+            // Check the query results are not empty
             if (studentIds.FirstOrDefault() == null)
             {
                 Console.WriteLine("Module code not found");
                 ReturnToMenu();
             }
+
+            // Iterate through query results and print them
             foreach (var absentStudent in studentNames)
             {
                 string fullname = absentStudent.studentForname + " " + absentStudent.studentSurname;
@@ -239,8 +292,12 @@ namespace Assignment01
         //(6)   Generate an attendance report for a selected module.
         public void GenerateReport(AttendanceModel dbC)
         {
+            Console.WriteLine("Attendance Report for module \n");
+
+            //Obtain moduleCode from the user
             string moduleCode = getModuleCode();
 
+            // Joining tables together to ensure the relevant information can be queried 
             var query =
             from attendanceA in dbC.Attendances
             join learningEventA in dbC.LearningEvents on attendanceA.eventID equals learningEventA.Id
@@ -258,15 +315,18 @@ namespace Assignment01
                 studentA.studentSurname
             };
 
+            // Check the query results are not empty 
             if (query.FirstOrDefault() == null)
             {
                 Console.WriteLine("Module code not found");
                 ReturnToMenu();
             }
 
+            // Creating the table
             var table = new ConsoleTable("Module Code", "Event Type", "Event Date/Time",
                 "Student Name", "Attendance Status");
 
+            // Iterate through the results and add them to rows, print the table
             foreach (var attendances in query)
             {
                 string fullname = attendances.studentForname + " " + attendances.studentSurname;
@@ -285,6 +345,7 @@ namespace Assignment01
         {
             Console.Write(msg);
             string input = Console.ReadLine();
+            Console.WriteLine("");
             while (!Regex.IsMatch(input, regex))
             {
                 Console.WriteLine("Invald Input");
